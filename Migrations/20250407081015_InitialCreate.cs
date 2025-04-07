e9d7ef1f94a9cb33120c83ec15cac0766927ef55
Nikos Methenitis <n.methenitis@indice.gr>
2025-04-07 11:14:10 +0300

    Add Migration

---
+++ Migrations/20250407081015_InitialCreate.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IrisPayments.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentCode",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrderID = table.Column<long>(type: "bigint", nullable: true),
+                    CustomerID = table.Column<long>(type: "bigint", nullable: false),
+                    OrderAmount = table.Column<double>(type: "float", nullable: true),
+                    IPAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "DateTime", nullable: false),
+                    ChangedAt = table.Column<DateTime>(type: "DateTime", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentCode", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PaymentLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderId = table.Column<long>(type: "bigint", nullable: true),
+                    CustomerId = table.Column<long>(type: "bigint", nullable: true),
+                    ReferenceId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LobId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrganizationId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TransactionId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaidAmount = table.Column<double>(type: "float", nullable: false),
+                    DebtorName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DebtorBankBIC = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PaymentDtTm = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BkDate = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RemittanceInformation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreationDtTm = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RejectionCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RejectionReason = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsPaid = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentCode");
+
+            migrationBuilder.DropTable(
+                name: "PaymentLogs");
+        }
+    }
+}
